2f6fd47a63973cfbc239c67fd51ed2968ad57d7f
Iffat Tehseen <iffat.tehseen1109@gmail.com>
2020-04-09 23:29:27 +0500

    removed folder

--- chat/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class signin(models.Model):
-    username    = models.CharField(max_length=20)
-    password    = models.CharField(max_length=20)
