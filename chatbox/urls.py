757ae0a112080f8798a0f99f8b68fe69a78c606a
Iffat Tehseen <iffat.tehseen1109@gmail.com>
2020-04-09 23:27:28 +0500

    removed folder from repo

--- chatbox/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include
-from django.urls import path
-from django.contrib import admin
-
-urlpatterns = [
-    path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
-]
